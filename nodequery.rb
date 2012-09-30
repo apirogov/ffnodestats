19718c35ea9449076388555ed384e4de5d29745d
Anton Pirogov <anton.pirogov at gmail.com>
2012-09-30 03:32:18 +0200

    Implemented higher level functions

---
+++ nodequery.rb
@@ -0,0 +1,105 @@
+#!/usr/bin/env ruby
+#NodeQuery - represents a slice of connection information in a given time span
+#needs a textfile database of connections and a nodes.json
+#to resolve multiple MACs to one node
+#Copyright (C) 2012 Anton Pirogov
+
+require 'date'
+require 'ffmaplib'
+
+class NodeQuery
+
+  attr_reader :cons
+  attr_reader :list
+
+  #initialize a NodeStat object to make requests
+  #which includes all entries for a given time span
+  #hash parameters: :fr=start Time (default: unixtime 0) :to=end Time (default: right now)
+  #:db = path to data files (default= ./db) :json=path or URI to nodes.json (default: burgtor.ffhl/mesh/nodes.json)
+  def initialize(hash={})
+    hash[:fr] = Time.at(0) if hash[:fr].nil?
+    hash[:to] = Time.now if hash[:to].nil?
+    hash[:db] = File.expand_path('./db') if hash[:db].nil?
+
+    fr = hash[:fr]
+    to = hash[:to]
+    db = hash[:db]
+    json = hash[:json]
+
+    frd = Time.new(fr.year,fr.month,fr.day,0,0).to_i
+    tod = Time.new(to.year,to.month,to.day,0,0).to_i
+
+    #get all entries of days touched in span
+    files = (Dir.entries(db)-['.','..']).map(&:to_i).select{|t| t>=frd && t<=tod}.map(&:to_s)
+
+    #convert to hashs and do fine filtering
+    @cons = files.map{|f| loadfile db+'/'+f}.inject(&:+).to_a.select{|c| c[:time]>=fr && c[:time]<=to}
+
+    json = ENV['NODEJSON_PATH'] if json.nil?
+    json = DEFAULT_NODESRC if json.nil?
+
+    @list = NodeWrapper.new json
+  end
+
+  #all known routers in that timespan
+  def routers
+    @cons.map{|c| c[:router]}.uniq.map{|m| n=@list[m]; n.nil? ? m : n.label}
+  end
+
+  #all known clients in that timespan
+  def clients
+    @cons.map{|c| c[:client]}.uniq
+  end
+
+  #all unique clients a router had in that timespan
+  def router_clients(router)
+    lbl = try_resolve router
+    @cons.select{|c| try_resolve(c[:router]) == lbl}.map{|c| c[:client]}.uniq
+  end
+
+  #all unique routers a client was connected to in that timespan
+  def client_routers(client)
+    @cons.select{|c| c[:client]==client}.map{|c| try_resolve c[:router]}.uniq
+  end
+
+  #get all connections of a client in that timespan
+  #if router name or mac given, only return connections to that router
+  def client_connections(client, router=nil)
+    cs = @cons.select{|c| c[:client]==client}.sort_by{|c| c[:time]}
+    cons = []
+    return cs if cs==[]
+
+    c = cs.shift
+    curr = {client: client, router: try_resolve(c[:router]), start: c[:time], end: c[:time]}
+    cs.each do |c|
+      #same router as last check and no gap of more than 20 minutes
+      if try_resolve(c[:router]) == curr[:router] && c[:time]-curr[:end]<1200
+        curr[:end] = c[:time]  #still connected -> change end-time
+      else  #changed routers or offline -> new connection entry
+        cons << curr
+        curr = {client: client, router: try_resolve(c[:router]), start: c[:time], end: c[:time]}
+      end
+    end
+    cons << curr  #put last one in
+
+    cons.select!{|c| c[:router] == try_resolve(router)} if !router.nil?
+    cons
+  end
+
+ # private
+
+  #filepath -> array of connection entry hashs
+  def loadfile(file)
+    File.readlines(file).map{|l| l.split(/\s+/)}.map do |l|
+      {time: Time.at(l[0].to_i), router: l[1], client: l[2]}
+    end
+  end
+
+  #try to resolve that router mac to a specific router known in nodes.json
+  #if not possible, return back that mac
+  def try_resolve(router)
+    return @list[router].label if @list[router]
+    router
+  end
+
+end
--- nodestat.rb
+++ nodestat.rb
@@ -1,102 +1,124 @@
 #!/usr/bin/env ruby
-#NodeStat - represents a slice of connection information in a given time span
-#needs a textfile database of connections and a nodes.json
-#to resolve multiple MACs to one node
+#NodeStat is the upper layer to request statistics
 #Copyright (C) 2012 Anton Pirogov
+#Licensed under the GPLv3
+#TODO: change backend to sqlite
 
-require 'date'
-require 'ffmaplib'
+require './nodequery.rb'
+
+#patches in helpers for dates
+class Time
+def self.today
+  t=Time.now
+  Time.new(t.year,t.month,t.day,0,0)
+end
+
+def self.yesterday
+  today-day_secs
+end
+
+def self.tomorrow
+  today+day_secs
+end
+
+def self.hour_secs
+  3600
+end
+
+def self.day_secs
+  86400
+end
+end
 
 class NodeStat
 
-  attr_reader :cons
-  attr_reader :list
+  @@default_start = Time.today
+  @@default_length = 24 #hours
 
-  #initialize a NodeStat object to make requests
-  #which includes all entries for a given time span
-  def initialize(hash={})
-    hash[:fr] = Time.at(0) if hash[:fr].nil?
-    hash[:to] = Time.now if hash[:to].nil?
-    hash[:db] = File.expand_path('./db') if hash[:db].nil?
-
-    fr = hash[:fr]
-    to = hash[:to]
-    db = hash[:db]
-    json = hash[:json]
-
-    frd = Time.new(fr.year,fr.month,fr.day,0,0).to_i
-    tod = Time.new(to.year,to.month,to.day,0,0).to_i
-
-    #get all entries of days touched in span
-    files = (Dir.entries(db)-['.','..']).map(&:to_i).select{|t| t>=frd && t<=tod}.map(&:to_s)
-
-    #convert to hashs and do fine filtering
-    @cons = files.map{|f| loadfile db+'/'+f}.inject(&:+).select{|c| c[:time]>=fr && c[:time]<=to}
-
-    json = ENV['NODEJSON_PATH'] if json.nil?
-    json = DEFAULT_NODESRC if json.nil?
-
-    @list = NodeWrapper.new json
+  #takes optional :db and :json parameters to be passed to NodeQuery
+  def initialize(h={})
+    @db = h[:db]
+    @json = h[:json]
   end
 
-  #all known routers in that timespan
-  def routers
-    @cons.map{|c| c[:router]}.uniq.map{|m| n=@list[m]; n.nil? ? m : n.label}
+  #Return all client connections to routers in given time (or for current day if no times given)
+  #client = client mac
+  #start = Time
+  #length = hours
+  def client_timeline(client, start=nil, length=nil)
+    query(start, length).client_connections client
   end
 
-  #all known clients in that timespan
-  def clients
-    @cons.map{|c| c[:client]}.uniq
+  #Return all client connections to this router in given time (or for current day if no times given)
+  #sorted by the time they started
+  #router = router mac or name
+  #start = Time
+  #length = hours
+  def router_timeline(router, start=nil, length=nil)
+    q = query(start, length)
+
+    clients = q.router_clients(router)
+    clients.map{|c| q.client_connections c, router}.inject(&:+).to_a.sort_by{|c| c[:start]}
   end
 
-  #all unique clients a router had in that timespan
-  def router_clients(router)
-    lbl = try_resolve router
-    @cons.select{|c| try_resolve(c[:router]) == lbl}.map{|c| c[:client]}.uniq
-  end
-
-  #all unique routers a client was connected to in that timespan
-  def client_routers(client)
-    @cons.select{|c| c[:client]==client}.map{|c| try_resolve c[:router]}.uniq
-  end
-
-  #get all connections of a client in that timespan
-  #if router name or mac given, only return connections to that router
-  def client_connections(client, router=nil)
-    cs = @cons.select{|c| c[:client]==client}.sort_by{|c| c[:time]}
-    cons = []
-
-    c = cs.shift
-    curr = {client: client, router: try_resolve(c[:router]), start: c[:time], end: c[:time]}
-    cs.each do |c|
-      #same router as last check and no gap of more than 20 minutes
-      if try_resolve(c[:router]) == curr[:router] && c[:time]-curr[:end]<1200
-        curr[:end] = c[:time]  #still connected -> change end-time
-      else  #changed routers or offline -> new connection entry
-        cons << curr
-        curr = {client: client, router: try_resolve(c[:router]), start: c[:time], end: c[:time]}
-      end
-    end
-    cons << curr  #put last one in
-
-    cons.select!{|c| c[:router] == try_resolve(router)} if !router.nil?
-    cons
-  end
-
- # private
-
-  #filepath -> array of connection entry hashs
-  def loadfile(file)
-    File.readlines(file).map{|l| l.split(/\s+/)}.map do |l|
-      {time: Time.at(l[0].to_i), router: l[1], client: l[2]}
+  #list of clients and how long in total (seconds) each was connected to a FF router for given time
+  def client_activity(start=nil, length=nil)
+    q = query(start,length)
+    clients = q.clients
+    clients.map do |c|
+      [c, q.client_connections(c).map{|c| c[:end]-c[:start]}.inject(&:+)]
     end
   end
 
-  #try to resolve that router mac to a specific router known in nodes.json
-  #if not possible, return back that mac
-  def try_resolve(router)
-    return @list[router].label if @list[router]
-    router
+  #list of routers and how many unique visitors they had in the given time
+  def unique_visitors(start=nil, length=nil)
+    q = query(start,length)
+    routers = q.routers
+    routers.map{|r| [r, q.router_clients(r).length]}
   end
 
+  #TODO: test after reimplementing backend with sqlite
+  def average_visitors(start=nil, length=nil)
+    q = query(start,length)
+    routers = q.routers
+    routers.map do |r|
+      loads = router_client_load r
+      avg = loads.map{|l| l[1]}.inject(&:+).to_f/loads.length
+      [r, avg]
+    end
+  end
+
+  #---- splits time into discrete slices ----
+
+  #returns an array of [Time,client_count] pairs
+  #router -> router name or mac
+  #start -> start time
+  #length -> length of timespan in hours
+  #step -> X scale in minutes (default: 30)
+  def router_client_load(router,step=30,start=nil,length=nil)
+    start=@@default_start if start.nil?
+    length=@@default_length if length.nil?
+    step = step.to_f/60.to_f #to hours
+    endtime = start+length*Time.hour_secs
+
+    ret = []
+    slice_start = start.dup
+    while slice_start < endtime
+      ret << [slice_start, query(slice_start,step).router_clients(router).length]
+      slice_start += step*Time.hour_secs
+    end
+    ret
+  end
+
+  private
+
+  #return NodeQuery object initialized correctly for given start and timespan length
+  #start and/or length can be nil and be set to sane defaults (whole currently running day if omitting both)
+  def query(start, length)
+    start=@@default_start if start.nil?
+    length=@@default_length if length.nil?
+    endtime = start+length*Time.hour_secs
+
+    return NodeQuery.new(fr: start, to: endtime, db: @db, json: @json)
+  end
 end
