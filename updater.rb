ee18e66d88446c4e4e8410f0b0ea7f22c4d27b94
Anton Pirogov <anton.pirogov at gmail.com>
2012-09-30 05:02:02 +0200

    Moved updater.rb from stupid file format to sqlite

--- updater.rb
+++ updater.rb
@@ -1,28 +1,27 @@
 #!/usr/bin/env ruby
 #script getting data for ffnodestats
 #Copyright (C) 2012 Anton Pirogov
-#Usage: DBPATH=/path/to/dir ruby updater.rb
-#Format:
-#the files in DBPATH represent all connection snapshots for a day
-#the file name is the unix time of the beginning of that day
-#a snapshot entry is:
-#timestamp AP-MAC Client-MAC
+#Usage: DBPATH=/path/to/db.db ruby updater.rb
 
+require 'sqlite3'
 require 'ffmaplib'
 
-dbpath = ENV['DBPATH']
+dbp = ENV['DBPATH']
 jsonpath = DEFAULT_NODESRC
 
-if dbpath.to_s.empty?
+if dbp.to_s.empty?
   puts "Please set DBPATH env var!"
   exit 1
 end
 
-if !`mkdir -p #{dbpath} 2>&1`.chomp.strip.empty?
-  puts "Failed to create DB directory!"
-  exit 1
+#init database if not existing
+if !File.exists?(dbp)
+  SQLite3::Database.new(dbp) do |db|
+    db.execute "CREATE TABLE connections (time INTEGER, router VARCHAR(17), client VARCHAR(17));"
+  end
 end
 
+#read data from json and generate connection rows
 timestamp = Time.now.to_i
 list = NodeWrapper.new jsonpath
 
@@ -31,7 +30,34 @@ list.online.routers.to_a.each do |r|
   r.clients.ids.each{|c| connections << "#{timestamp} #{r.id} #{c}"}
 end
 
-todayfile = Time.new(Time.now.year, Time.now.month, Time.now.day, 0, 0).to_i.to_s
-File.open(dbpath+'/'+todayfile,'a') do |f|
-  connections.each{|c| f.puts c}
+#write new connections in 500 row blocks to database
+db = SQLite3::Database.new(dbp)
+
+#template for insert
+cmd = "INSERT INTO connections (time, router, client) VALUES "
+
+curr = cmd
+counter = 0
+
+connections.each do |r|
+  r=r.split(/\s+/)
+  curr += "(#{r[0]}, \"#{r[1]}\", \"#{r[2]}\"),"
+  counter += 1
+
+  if counter == 500
+    puts "block"
+    curr[-1]=";"  #close command
+    db.execute curr
+
+    counter = 0
+    curr = cmd
+  end
 end
+
+#write rest if any
+if counter != 0
+  curr[-1]=";"
+  db.execute curr
+end
+
+db.close
